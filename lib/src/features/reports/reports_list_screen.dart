7b543604a29c15a78fb175956230cce9dec3b585
Kelsey Hartman <kelsey@bryzos.com>
2026-06-18 12:17:20 -0500

    fix: add missing kIsWeb and url_launcher imports for web export

--- lib/src/features/reports/reports_list_screen.dart
+++ lib/src/features/reports/reports_list_screen.dart
@@ -18,9 +18,11 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../config/feature_flags.dart';
 import '../../data/models.dart';
